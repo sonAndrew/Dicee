4ccacc70ed4425ef07ba22e459ef623124269b1b
sonandrew <andrewz.jackson667@gmail.com>
2020-12-12 10:17:32 -0600

    Created a function to handle the number randomization and dice view assignment.

--- app/src/main/java/com/example/dicee/MainActivity.java
+++ app/src/main/java/com/example/dicee/MainActivity.java
@@ -17,17 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Variable Declarations
+                                                                        // Variable Declarations
         Button rollButton;
         ImageView leftDice;
         ImageView rightDice;
         final int[] diceArray;
-
-        // Variable Assignments
+                                                                        // Variable Assignments
         rollButton = (Button) findViewById(R.id.rollButton);
-
         leftDice = (ImageView) findViewById(R.id.image_leftDice);
-
         rightDice = (ImageView) findViewById(R.id.image_rightDice);
 
         diceArray = new int[] {
@@ -45,19 +42,35 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.d("Dicee", "Hazaaah! Ze Button has been pressed!");
 
-                Random randomNumberGenerator = new Random();
+//                Random randomNumberGenerator = new Random();
+//
+//                int number = randomNumberGenerator.nextInt(6);
+//                Log.d("Dicee", "The random number is: " + number);
+//
+//                leftDice.setImageResource(diceArray[number]);
+//
+//                number = randomNumberGenerator.nextInt(6);
+//                Log.d("Dicee", "The random number is: " + number);
+//
+//                rightDice.setImageResource(diceArray[number]);
 
-                int number = randomNumberGenerator.nextInt(6);
-                Log.d("Dicee", "The random number is: " + number);
-
-                leftDice.setImageResource(diceArray[number]);
-
-                number = randomNumberGenerator.nextInt(6);
-                Log.d("Dicee", "The random number is: " + number);
-
-                rightDice.setImageResource(diceArray[number]);
+                randomizer(leftDice, diceArray);
+                randomizer(rightDice, diceArray);
             }
         });
+
+    }
+                                                                        // This function is used to randomize a dice
+    public void randomizer(ImageView dice, int[] array){
+                                                                        // Variable Declarations
+        int number;
+                                                                        // Creates a new random number generator
+        Random randomNumberGenerator = new Random();
+                                                                        // Assigns a random number from 1 - 6 to the "number" variable
+        number = randomNumberGenerator.nextInt(6);
+                                                                        // Sets the image view in the array to the randomly generated number
+        dice.setImageResource(array[number]);
+        Log.d("Dicee", "The random number is: " + number);
 
     }
 }
